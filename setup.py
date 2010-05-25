47e018f7c5481c4b9395143425dfeb12f5548ce7
Ask Solem <askh@opera.com>
2010-05-25 10:50:30 +0200

    Forgot to set version in setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ else:
 
 setup(
     name='nose-cover3',
-    version="0.0.4",
+    version="0.0.5",
     description="Coverage 3.x support for Nose",
     author="Ask Solem",
     author_email="askh@opera.com",
